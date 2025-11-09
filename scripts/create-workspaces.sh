a85e4460ab1f4d7073a4d2c4cd7d50ce36cf79b6
OmkarHosavalike <homkar8@gmail.com>
2025-11-09 12:17:04 +0530

    Test 3

--- scripts/create-workspaces.sh
+++ scripts/create-workspaces.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")/../terraform"
 
 for ws in dev prod; do
-    terraform init -input=false -backend-config="bucket=${bucket}" -backend-config="key=${ws}/terraform.tfstate"
+    terraform init -input=false -backend-config="bucket=${bucket}" -backend-config="key=${ws}/terraform.tfstate" -reconfigure
     if terraform workspace list | grep -qE "\\b${ws}\\b"; then
         terraform workspace select "$ws"
     else
