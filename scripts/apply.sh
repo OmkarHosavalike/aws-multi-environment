9208d404a3b825012f6758032870489847d8acbf
OmkarHosavalike <homkar8@gmail.com>
2025-11-09 12:59:35 +0530

    Test 5

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 ws="$1"
 cd "$(dirname "$0")/../terraform"
+  
+terraform workspace select "$ws" || { echo "Workspace $ws not available" ; exit 1; }
 
 terraform init -input=false -no-color -backend-config="bucket=${bucket}" \
-    -backend-config="key=${TF_WORKSPACE}/terraform.tfstate"
-    
-terraform workspace select "$ws" || terraform workspace new "$ws"
+    -backend-config="key=${ws}/terraform.tfstate"
 
 if [ -f "${ws}.tfvars" ]; then
     terraform apply -var-file="${ws}.tfvars" --auto-approve
 else
-    terraform apply --auto-approve
+    echo "environment specific tfvars file doesn't exist"
+    exit 1;
 fi
