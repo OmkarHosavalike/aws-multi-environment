3695b35abc9126be3b2b5fb5a614fcb037aa7b29
OmkarHosavalike <homkar8@gmail.com>
2025-11-09 13:18:14 +0530

    test...

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,11 +2,11 @@
 ws="$1"
 cd "$(dirname "$0")/../terraform"
   
-terraform workspace select "$ws" || { echo "Workspace $ws not available" ; exit 1; }
-
 terraform init -input=false -no-color -backend-config="bucket=${bucket}" \
     -backend-config="key=terraform.tfstate"
     #-backend-config="key=${ws}/terraform.tfstate"
+
+terraform workspace select "$ws" || { echo "Workspace $ws not available" ; exit 1; }
 
 if [ -f "${ws}.tfvars" ]; then
     terraform apply -var-file="${ws}.tfvars" --auto-approve
